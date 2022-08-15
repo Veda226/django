563896cc04a4729a79a653831fc04f3710c0d3c4
Veda226 <veda226@gmail.com>
2022-08-15 21:02:58 +0530

    initial commit

---
+++ admin/products/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializers
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): # /api/products
+        products = Product.objects.all()
+        serializer = ProductSerializers(products,many = True)
+        return Response(serializer.data)
+
+    def create(self, request):# /api/products
+        pass
+
+    def retrieve(self, request,pk = None):# /api/products/<str:id>
+        pass
+
+    def update(self, request,pk = None):# /api/products/<str:id>
+        pass
+
+    def destroy(self, request,pk = None):# /api/products/<str:id>
+        pass
